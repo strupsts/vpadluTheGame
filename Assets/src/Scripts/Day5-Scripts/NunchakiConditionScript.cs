0ee640210795d636ae9afa6c6fc89ab67a2afe77
Ilyasandroidpvp <evilleviam@gmail.com>
2024-07-17 11:54:22 +0300

    Day 5 Vova scene

---
+++ Assets/src/Scripts/Day5-Scripts/NunchakiConditionScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NunchakiConditionScript : MonoBehaviour, ICustomTrigger
+{
+    public void Start()
+    {
+        Debug.Log($"Нунчаки?? {GameManager.Instance.GetSomeThingsListValue("Nunchaki")}");
+    }
+    public void Trigger()
+    {
+        Debug.Log("Получен новый предмет: Нунчаки");
+        GameManager.Instance.SetSomeThingsListValue("Nunchaki", true);
+        GameManager.Instance.notifyItemHandler(true,"Нунчаки");
+        Debug.Log(GameManager.Instance.GetSomeThingsListValue("Nunchaki"));
+    }
+
+    
+}
