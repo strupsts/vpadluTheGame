2c1cf9bc03b6148c399764068893ce51fb05b882
Strupsts <strupsts@mail.ru>
2024-11-07 14:36:51 -0600

    PredFilterGit

--- Assets/src/Scripts/MenuScripts/playButton.cs
+++ Assets/src/Scripts/MenuScripts/playButton.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -43,7 +44,7 @@ public class playButton : MonoBehaviour
     {
         if (!SaveManager.checkSaves())
         {
-            SceneManager.LoadScene(28);
+            SceneManager.LoadScene(1);
         }
         else 
         {
@@ -55,11 +56,27 @@ public class playButton : MonoBehaviour
     public void ClearAllData()
     {
         PlayerPrefs.DeleteAll();
-        GameManager.someThingsList.Clear();
-        GameManager.reputationList.Clear();
-        GameManager.currentScene = 0;
-        Quiz.QuizPointsList.Clear();
+        foreach (var key in GameManager.someThingsList.Keys.ToList())
+        {
+            GameManager.someThingsList[key] = false;
+        }
+        Debug.Log("Восстановили значения someThingsList на FALSE");
+        foreach (var key in GameManager.reputationList.Keys.ToList())
+        {
+            if (key != "Company") GameManager.reputationList[key] = 50;
+            else
+            { 
+                GameManager.reputationList[key] = 100;
+                Debug.Log("С компанией восстановили репутацицю: 100 теперь.");
+            }
+        }
 
+        GameManager.currentScene = 0;
+        foreach (var key in Quiz.QuizPointsList.Keys.ToList())
+        {
+            Quiz.QuizPointsList[key] = 0;
+        }
+        Debug.Log("Восстановили значения QUIZ POINTS на 0 все");
         btn.GetComponentInChildren<TextMeshProUGUI>().text = "Новая игра";
     }
 }
